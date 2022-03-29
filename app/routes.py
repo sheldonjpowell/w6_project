6c507d383166080c29edcded227845482094a4db
sheldonjpowell <sheldonjpowell@yahoo.com>
2022-03-29 11:07:26 -0500

    New files

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,46 @@
 from app import app
-
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello World!!!!!'
-    
+    title = 'Home'
+    user = {'id': 1, 'username': 'bstanton', 'email': 'brians@codingtemple.com'}
+    posts = [
+        {
+            'id': 1,
+            'title': 'March Madness',
+            'body': 'The NCAA tournament never seems to disappoint!',
+            'author': 'cbbfan'
+        },
+        {
+            'id': 2,
+            'title': 'Oscars',
+            'body': 'I think that Coda might win Best Picture this year. I cried the whole time.',
+            'author': 'BuffMovieBuff123'
+        },
+        {
+            'id': 3,
+            'title': 'Python',
+            'body': 'Python is by far my favorite programming language. It is so neat!',
+            'author': 'code_is_life'
+        },
+        {
+            'id': 4,
+            'title': 'Flask',
+            'body': 'My favorite microframework! And this is only the beginning.',
+            'author': 'CodingTemple4L'
+        }
+    ]
+    return render_template('index.html', current_user=user, title=title, posts=posts)
 
-@app.route('/test')
-def test():
-    return 'This is the TEST!!'
+
+@app.route('/signup')
+def signup():
+    title = 'Sign Up'
+    return render_template('signup.html', title=title)
+
+
+@app.route('/login')
+def login():
+    title = 'Log In'
+    return render_template('login.html', title=title)
