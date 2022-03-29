461ce7d47cadd4413515af0424e9d4f475eaf199
sheldonjpowell <sheldonjpowell@yahoo.com>
2022-03-29 14:28:39 -0500

    added flask sqlalchemy, flask-migrate

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,11 @@
+from ast import Pass
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 class SignUpForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), Email()])
-    username = StringField('username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
 from app import app
 from flask import render_template
-from app.forms import SignUpForms
-
+from app.forms import SignUpForm
 
 @app.route('/')
 def index():
@@ -36,10 +35,18 @@ def index():
     return render_template('index.html', current_user=user, title=title, posts=posts)
 
 
-@app.route('/signup')
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
     title = 'Sign Up'
     form = SignUpForm()
+    # check if a post request and that the form is valid
+    if form.validate_on_submit():
+        # Get data from the validated form
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        # Print the data
+        print(email, username, password)
     return render_template('signup.html', title=title, form=form)
 
 
